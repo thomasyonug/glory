b1d2354678aa02fc988a0ca59f48b01d0e656ebd
杨文章 <yangwz@citycloud.com.cn>
2017-05-12 23:47:34 +0800

    Add god combine

--- src/reduxs/reducer/index.js
+++ src/reduxs/reducer/index.js
@@ -1,13 +1,19 @@
 import {combineReducers} from 'redux'
-import storeCards from './storeCards'
-import e_storeCards from './e_storeCards'
-import handCards from './handCards'
-import e_handCards from './e_handCards'
+import storeCards        from './storeCards'
+import e_storeCards      from './e_storeCards'
+import handCards         from './handCards'
+import e_handCards       from './e_handCards'
+import god               from './god'
+
+
+
+
 export default combineReducers({
     storeCards,
     e_storeCards,
     handCards,
-    e_handCards
+    e_handCards,
+    god
 })
 
 
