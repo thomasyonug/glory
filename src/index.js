e7d6f277ed57df45570df9445f7ee11d99aafea4
thomasyonug <563473579@qq.com>
2017-05-16 14:23:25 +0800

    Add babel plugin to enable es6 dynamic-import

--- src/index.js
+++ src/index.js
@@ -6,20 +6,22 @@ import './index.css';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from 'reduxs/reducer'
-
 const store = createStore(reducer)
 
 //mock
 if (process.env.NODE_ENV === 'development') {
-  const fetchMock = require('fetch-mock')
-  const templateData = require('./mock')
-  Object.keys(templateData).forEach(key => {
-    const {
-      method,
-      url,
-      data
-    } = templateData[key]
-    fetchMock[method](url, data)
+  Promise.all([
+    import('fetch-mock'),
+    import('./mock')
+  ]).then(([fetchMock, templateData]) => {
+    Object.keys(templateData).forEach(key => {
+      const {
+        method,
+        url,
+        data
+      } = templateData[key]
+      fetchMock[method](url, data)
+    })
   })
 }
 
