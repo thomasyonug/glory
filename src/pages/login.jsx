131b2375ed97aef153122497aebef4b5dac649ed
yesunfei <yesunfei@yesunfeideMac.local>
2017-05-16 17:29:23 +0800

    update login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,27 +2,33 @@ import React, {Component} from 'react'
 import CSSModules from 'react-css-modules'
 import {Link} from 'react-router-dom'
 import styles from './login.css'
-
-
-
-
-
+import  'whatwg-fetch'
 
 @CSSModules(styles)
 export default class Login extends Component{
     constructor(props) {
         super(props);
-        this.state={username:'',password:''};
+        this.state = {username:'',password:''};
     }
-    login() {
+    login = () => {
         let username = this.state.username;
         let password = this.state.password;
-        alert("user: "+username + ",password: "+password)
+        let data = { "username":username,"password":password };
+
+        fetch('/api/login', {
+          method: 'POST',
+          body: data
+        }).then(res => {
+            return res.text()
+        }).then(text => {
+            console.log(text);
+            window.location.href = "http://localhost:3000/#/game";
+        })
     }
-    handleUserName(e){
+    handleUserName = (e) => {
         this.setState({username: e.target.value});
     }
-    handlePassWord(e){
+    handlePassWord = (e) => {
         this.setState({password: e.target.value});
     }
 
@@ -36,17 +42,17 @@ export default class Login extends Component{
                 
             // </div>
 
-            <form styleName="login_content">
+            <div styleName="login_content">
                 <div>
                     <label >用户名:</label>
-                    <input  name="username" onChange={this.handleUserName.bind(this)} />
+                    <input  name="username" onChange={this.handleUserName} />
                 </div>
                 <div>
                     <label >密码:</label>
-                    <input  name="password" type="password" onChange={this.handlePassWord.bind(this)} />
+                    <input  name="password" type="password" onChange={this.handlePassWord} />
                 </div>
-                <button styleName="login_btn" onClick={this.login.bind(this)}>登陆</button>
-            </form>
+                <button styleName="login_btn" onClick={this.login}>登陆</button>
+            </div>
             
         )
     }
