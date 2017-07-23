9af08b17411b33b933e0fc64fd8dbfc975dbf7dd
thomasyonug <563473579@qq.com>
2017-07-23 13:54:17 +0800

    Summon finish

--- src/middleware/epic/roundGuard/index.js
+++ src/middleware/epic/roundGuard/index.js
@@ -3,22 +3,13 @@ import {
     ROUND_NAME_GET_CARD_REGULAR
 } from 'reduxs/constant'
 
-import { Observable } from 'rxjs/Observable';
 
 export default (action$, store) =>
   action$.ofType(NEXT_ROUND)
-    .mergeMap(action => {
-        const currentRound = store.getState().god.roundState
-
-        switch (currentRound) {
-            case ROUND_NAME_GET_CARD_REGULAR:
-                return Observable.of(new window.Transer({
-                    glory: 'get_cards_from_store_to_hand',
-                    content: {
-                        start: -1
-                    }
-                }))
-            default:
-                return Observable.of({})
+    .filter(action => store.getState().god.roundState === ROUND_NAME_GET_CARD_REGULAR)
+    .map(action => new window.Transer({
+        glory: 'get_cards_from_store_to_hand',
+        content: {
+            start: -1
         }
-    })
+    }))
