c2e34781de5c0b11254f2e3013ffe0cfe7c489b2
thomasyonug <563473579@qq.com>
2017-08-01 23:37:46 +0800

    update webpack & babel

--- src/middleware/hook/deadBefore.js
+++ src/middleware/hook/deadBefore.js
@@ -1,14 +1,20 @@
 import {
-    // DROP_MONSTER_CARDS_FROM_BATTLEFIELD,
-    // DROP_MONSTER_CARDS_FROM_E_BATTLEFIELD
+    DROP_MONSTER_CARDS_FROM_BATTLEFIELD,
+    DROP_MONSTER_CARDS_FROM_E_BATTLEFIELD
 } from 'reduxs/constant'
 
 
 export default function (store, next, action) {
-    // const stateSnapshot = store.getState()
-    // const {
-    //     battleField,
-    //     e_battleField
-    // } = stateSnapshot
+    const stateSnapshot = store.getState()
+
+    const {
+        battleField,
+        e_battleField
+    } = stateSnapshot
+
+    if (action.type === DROP_MONSTER_CARDS_FROM_BATTLEFIELD) {
+        const monster = battleField[action.content.index]
+        monster.deathwhisper?.()
+    }
 
 }
