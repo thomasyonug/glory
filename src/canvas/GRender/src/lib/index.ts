b337b4a1ae81a19159ca00dd2ca63d88a4f18774
thomasyonug <563473579@qq.com>
2017-08-16 11:06:59 +0800

    add loader

--- src/canvas/GRender/src/lib/index.ts
+++ src/canvas/GRender/src/lib/index.ts
@@ -1,7 +1,9 @@
 import Sprite    from './Sprite'
 import Stage     from './Stage'
+import Loader    from './Loader'
+import {applyMixins} from '../util/index'
 
-export default class GRender {
+export default class GRender implements Loader{
     static Sprite = Sprite;
 
     public sprites: Sprite[] = [];
@@ -20,6 +22,10 @@ export default class GRender {
             )
         })
     }
+
+    public loadImg: (url: string) => Promise<HTMLImageElement> 
+
+
 
     public render(): () => void {
         return this.stage.render()
@@ -44,7 +50,16 @@ export default class GRender {
         this.stage.nextTick(fn)
     }
 
+    
+    public async drawImg (url: string): Promise<HTMLImageElement> {
+        const imgEl = await this.loadImg(url)
+
+        this.ctx.drawImage(imgEl, 0, 0, imgEl.width, imgEl.height, 0, 0, this.stage.width, this.stage.height)
+
+        return imgEl
+    }
 }
+applyMixins(GRender, [Loader]);
 
 export {
     Sprite
