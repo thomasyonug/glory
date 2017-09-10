f30ed001df0dcb03d30537b28623e14751ce483f
thomasyonug <563473579@qq.com>
2017-09-10 22:41:24 +0800

    modify

--- src/resource/cards/trap/burstArmor.js
+++ src/resource/cards/trap/burstArmor.js
@@ -1,11 +1,21 @@
 import {TrapEntity} from './trapEntity'
 import {prototype} from 'decorators'
+import {
+    PUSH_ATTACK_BEFORE_TRIGGER,
+    DELETE_ATTACK_BEFORE_TRIGGER
+} from 'reduxs/constant'
 
 @prototype({
     describe: '受到攻击时破坏攻击怪兽',
     cardName: '爆裂装甲',
     setTrigger (store, action) {
-        console.log('fuckfuckfuck')
+        store.dispatch({
+            type: PUSH_ATTACK_BEFORE_TRIGGER,
+            content (arg,store) {
+                debugger
+                console.log(arg)
+            }
+        })
     }
 })
 export class BurstArmorTrap extends TrapEntity {
